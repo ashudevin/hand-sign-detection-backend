64040282d50074ce316376da47c28d1d6c615746
Arshdeep Singh <142790426+ashudevin@users.noreply.github.com>
2025-03-24 14:29:17 +0530

    change origin

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://sign-detection-frontend.onrender.com", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
